e79985a22d259ed6a826d52a6dcb2e006a8cd6be
Rana123Mandeep <writetomandeeprana@gmail.com>
2024-09-05 10:10:10 +1200

    addes setting for Aws in codebase

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
  
 const userRouter = require('./routes/userAPI');
+ // require('dotenv').config(); //for using variables from .env file.
+require('./config/config');
+
+console.log(process.env.NODE_ENV);
  
-require('dotenv').config(); //for using variables from .env file.
- 
+console.log(process.env.MONGODB_URL);
 // Set up the express app
 
 const app = express(); //instantiates the express object
@@ -32,6 +35,29 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 
 });
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+ 
+//This enabled CORS, Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources (e.g. fonts) 
+//on a web page to be requested from another domain outside the domain from which the first resource was served
+ 
+app.all('/*', function(req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method == 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+ 
+module.exports = app;
+
+
 
 app.use('/users',userRouter);
  
